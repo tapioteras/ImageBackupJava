9ff0bd135e29e899ff8e7c3d0b1488d13b94fc42
Tapio Teras <tapio.teras@siili.com>
2017-06-05 20:44:02 +0300

    started using logback

--- src/main/java/fi/teras/ImageBackupHelper.java
+++ src/main/java/fi/teras/ImageBackupHelper.java
@@ -10,6 +10,8 @@ import de.vandermeer.asciitable.v2.V2_AsciiTable;
 import de.vandermeer.asciitable.v2.render.V2_AsciiTableRenderer;
 import de.vandermeer.asciitable.v2.render.WidthAbsoluteEven;
 import de.vandermeer.asciitable.v2.themes.V2_E_TableThemes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.imageio.ImageIO;
 import java.io.File;
@@ -26,6 +28,7 @@ import java.util.stream.Stream;
 public class ImageBackupHelper {
     private List<Path> succeeded = new ArrayList<>();
     private List<Path> failed = new ArrayList<>();
+    private static final Logger log = LoggerFactory.getLogger("fi.teras.ImageBackupHelper");
 
     private final static Map<Integer, String> MONTH_FOLDER_NAME_MAP = Collections.unmodifiableMap(
             Stream.of(new AbstractMap.SimpleEntry<>(0, "tammi"), new AbstractMap.SimpleEntry<>(1, "helmi"), new AbstractMap.SimpleEntry<>(2, "maalis"),
@@ -35,11 +38,11 @@ public class ImageBackupHelper {
                     .collect(Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue())));
 
     public void backupImages(Path from, Path to) throws IOException {
-        System.out.println("from: " + from + ", to: " + to + "\n");
+        log.info("from: " + from + ", to: " + to + "\n");
         Collection<Path> images = getImages(from);
         images.stream().forEach(imagePath -> {
             try {
-                System.out.println("\n\n----- Processing file " + imagePath + "... -----");
+                log.info("\n\n----- Processing file " + imagePath + "... -----");
                 resolvePathTo(imagePath, to);
                 succeeded.add(imagePath);
             } catch (Exception e) {
@@ -90,7 +93,7 @@ public class ImageBackupHelper {
             Date dateTaken = getFileMimeType(imagePath.toFile()).getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
             Calendar cal = Calendar.getInstance();
             cal.setTime(dateTaken);
-            System.out.println("Got date taken " + dateTaken + " / " + imagePath.getFileName());
+            log.debug("Got date taken " + dateTaken + " / " + imagePath.getFileName());
             return cal;
         } catch (Exception e) {
             return null;
@@ -103,7 +106,7 @@ public class ImageBackupHelper {
             attr.creationTime().toInstant();
             Calendar cal = Calendar.getInstance();
             cal.setTime(new Date(attr.creationTime().toInstant().toEpochMilli()));
-            System.out.println("Got file created time " + attr.creationTime() + " / " + filePath.getFileName());
+            log.debug("Got file created time " + attr.creationTime() + " / " + filePath.getFileName());
             return cal;
         } catch (IOException e) {
             return null;
@@ -124,40 +127,47 @@ public class ImageBackupHelper {
         return getCreationDate(filePath) != null;
     }
 
+    private int getSucceededSize() {
+        return succeeded.size() > 0 ? succeeded.size() : 1;
+    }
+
     private Double countSuccessPercent() {
         int totalCount = succeeded.size() + failed.size();
-        return Double.valueOf(100 * (totalCount - failed.size()) / succeeded.size());
+        return Double.valueOf(100 * (totalCount - failed.size()) / getSucceededSize());
     }
 
     private void printSummary() {
-        System.out.println("\n\n##### Image backup completed #####");
-        V2_AsciiTable at = new V2_AsciiTable();
-        at.addRule();
-        at.addRow("Success percent", countSuccessPercent() + " %");
-        at.addRule();
-        at.addRow("Succeeded", succeeded.size() + " files");
-        at.addRule();
-        at.addRow("Failed", failed.size() + " files");
-        at.addStrongRule();
-        at.addRow("Total", succeeded.size() + failed.size() + " files");
-        at.addRule();
-        V2_AsciiTableRenderer rend = new V2_AsciiTableRenderer();
-        rend.setTheme(V2_E_TableThemes.UTF_LIGHT.get());
-        rend.setWidth(new WidthAbsoluteEven(50));
-        RenderedTable rt = rend.render(at);
-        System.out.println(rt);
+        try {
+            log.info("\n\n##### Image backup completed #####");
+            V2_AsciiTable at = new V2_AsciiTable();
+            at.addRule();
+            at.addRow("Success percent", countSuccessPercent() + " %");
+            at.addRule();
+            at.addRow("Succeeded", succeeded.size() + " files");
+            at.addRule();
+            at.addRow("Failed", failed.size() + " files");
+            at.addStrongRule();
+            at.addRow("Total", succeeded.size() + failed.size() + " files");
+            at.addRule();
+            V2_AsciiTableRenderer rend = new V2_AsciiTableRenderer();
+            rend.setTheme(V2_E_TableThemes.UTF_LIGHT.get());
+            rend.setWidth(new WidthAbsoluteEven(50));
+            RenderedTable rt = rend.render(at);
+            log.info(String.valueOf(rt));
+        } catch (Exception e) {
+            log.warn("Image backup summary cannot be printed because exception: " + e.getMessage());
+        }
     }
 
     private void resolvePathTo(Path imagePath, Path to) throws IOException {
         Calendar cal;
-
         if (hasDateTaken(imagePath)) {
             cal = getDateTaken(imagePath);
         } else if (hasCreationTime(imagePath)) {
-            System.out.println("No date taken found, trying to find file created time...");
+            log.debug("No date taken found, trying to find file created time...");
             cal = getCreationDate(imagePath);
         } else {
-            System.out.println("File created time not found");
+            log.debug("File created time not found");
             throw new NullPointerException("no suitable date found");
         }
         copy(to, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), imagePath);
--- src/main/java/fi/teras/ImageBackupRunner.java
+++ src/main/java/fi/teras/ImageBackupRunner.java
@@ -1,15 +1,20 @@
 package fi.teras;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
 
 public class ImageBackupRunner {
+    private static final Logger log = LoggerFactory.getLogger("fi.teras.ImageBackupRunner");
+
     static public void main(String[] args) {
         try {
             ImageBackupHelper helper = new ImageBackupHelper();
             helper.backupImages(new File(args[0]).toPath(), new File(args[1]).toPath());
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error(String.valueOf(e));
         }
     }
 }
